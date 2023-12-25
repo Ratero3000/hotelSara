fbdba9ea9e6e1bd94379bf73f5c781d541808351
Samuel Antonio Ramos <saanraal.0610@gmail.com>
2023-12-24 22:38:53 -0600

    Este es mi primer commit

---
+++ src/main/java/com/Hotel/HotelSARA/HotelSaraApplication.java
@@ -0,0 +1,13 @@
+package com.Hotel.HotelSARA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hotel/HotelSARA/HotelSaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hotel.HotelSARA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
